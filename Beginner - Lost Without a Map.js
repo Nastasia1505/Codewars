8be0fdaa2d41a170e93badeaf2f16e9455fd858a
_Nastasia_ <nastya.svirid@mail.ru>
2022-10-24 16:34:30 +0300

    Exercise

---
+++ Beginner - Lost Without a Map.js
@@ -0,0 +1,12 @@
+//Given an array of integers, return a new array with each value doubled.
+
+
+// const x = [1, 3, 5];
+
+// function maps(x){
+// const b = x.map((item)=>{
+//     return item*2;
+    
+// })
+// console.log(b)
+// }
